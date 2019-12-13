bcd6357dd1d4f3628ed0a1210cdc2c55d16a5703
habonn <teeruthabon@gmail.com>
2019-12-13 22:33:17 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
+import Movieitem from './Movieitem';
 
-function App() {
-  return (
+
+
+class App extends Component {
+
+    constructor(props){
+      super(props)
+
+      this.state ={rows: []}
+    }
+
+  search = (keyword)=>{
+    console.log(keyword)
+    var dataArray = []
+    var url = "https://api.themoviedb.org/3/search/movie?api_key=33bd94334d538dbf614968c65c83250b&query=" + keyword;
+    Axios.get(url).then(result=>{
+      console.log(JSON.stringify(result.data.results))
+      result.data.results.forEach(item => {
+        item.poster_src = "https://image.tmdb.org/t/p/w185" + item.poster_path
+        dataArray.push(item)
+      })
+
+      this.setState({rows: dataArray});
+    })
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <table className="NavBar">
+      <tbody>
+        <tr>
+        <td>
+            <img src={require('./logo.svg')} width="50"/>
+          </td>
+          <td>
+            Hello React
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <input style={{fontSize: 24, display: 'block', width: '100%', paddingLeft: 8}} placeholder="Enter your movie keyword"
+    onChange={(event)=> { this.search(event.target.value)}}/>
+    
+    { this.state.rows.map(item=>
+        <Movieitem movie={item} />
+     )}
+
+
     </div>
+
+
+
   );
-}
+  }
+  
+} 
+
+
 
 export default App;
---
+++ src/Movieitem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Movieitem extends Component {
+    
+    constructor(props){
+        super(props)
+    }
+    
+    render() {
+
+        const {id, title, overview, vote_average, poster_src} = this.props.movie
+        return (
+            <table key={id} style={{textAlign: 'left'}}>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src={poster_src} />
+                        </td>
+
+                        <td>
+                            <strong>{title}</strong>
+                            <p> {overview} </p>
+                            <p> <b>IMDb Rating</b> : {vote_average} </p>
+
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+}
